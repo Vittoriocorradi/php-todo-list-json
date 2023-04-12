d89977d474c8573b76c43c7d8c4ef3dd059162f9
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-04-12 15:03:56 +0200

    Bootstrap layout

--- index.php
+++ index.php
@@ -18,8 +18,26 @@
 
 <body>
     <!-- Vue Container -->
-    <div id="app">
-
+    <div id="app" class="p-5">
+        <div class="row justify-content-center gy-4">
+            <div class="col-7">
+                <h1 class="text-center">Todo List</h1>
+            </div>
+            <div class="col-7">
+                <ul class="list-group">
+                    <li class="list-group-item">An item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                </ul>
+            </div>
+            <div class="col-7">
+                <div class="input-group mb-3">
+                    <label for="input-text"></label>
+                    <input type="text" class="form-control" placeholder="Inserisci elemento..." id="input-text" aria-describedby="button">
+                    <button class="btn btn-outline-secondary" id="button">Button</button>
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- My JS -->
